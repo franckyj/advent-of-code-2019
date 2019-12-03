fcc4a89c59f2b3185f1addad707b8f54d624d71a
Francois Joly <joly.francois+github@gmail.com>
2019-12-03 16:34:05 -0500

    add missing files!

--- advent-of-code/Program.cs
+++ advent-of-code/Program.cs
@@ -9,8 +9,9 @@ namespace advent_of_code
     {
         static void Main(string[] args)
         {
-            Day1();
-            Day2();
+            //Day1();
+            //Day2();
+            Day3();
         }
 
         #region day1
@@ -131,6 +132,191 @@ namespace advent_of_code
         private static void WriteResult(int day, int puzzle, object result)
         {
             Console.WriteLine($"day {day} puzzle {puzzle}: {result}");
+        }
+
+        #endregion
+
+        #region day3
+
+        private static void Day3()
+        {
+            var input = new StreamReader("inputs/puzzle-3-1.txt").ReadToEnd();
+            var result1 = Day3Puzzle1(input);
+            WriteResult(3, 1, result1);
+
+            var result2 = Day3Puzzle2();
+            WriteResult(1, 2, result1);
+        }
+
+        private static int Day3Puzzle1(string input)
+        {
+            var split = input.Split(Environment.NewLine);
+            var input1 = split[0];
+            var input2 = split[1];
+
+            var positions1 = GetAllPositions(input1);
+            var positions2 = GetAllPositions(input2);
+
+            positions1.IntersectWith(positions2);
+
+            var minDistance = int.MaxValue;
+            foreach (var pos in positions1)
+            {
+                var distance = Math.Abs(pos.X) + Math.Abs(pos.Y);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                }
+            }
+            return minDistance;
+        }
+
+        private static int Day3Puzzle2(string input)
+        {
+            var split = input.Split(Environment.NewLine);
+            var input1 = split[0];
+            var input2 = split[1];
+
+            var positions1 = GetAllPositions(input1);
+            var positions2 = GetAllPositions(input2);
+
+            positions1.IntersectWith(positions2);
+
+            var minDistance = int.MaxValue;
+            foreach (var pos in positions1)
+            {
+                var distance = Math.Abs(pos.X) + Math.Abs(pos.Y);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                }
+            }
+            return minDistance;
+        }
+
+        private static HashSet<Position> GetAllPositions(string input)
+        {
+            var stack = new Stack<char>();
+            var span = input.AsSpan();
+            var list = new HashSet<Position>();
+            var currentPosition = new Position(0, 0);
+            foreach (var c in span)
+            {
+                if (c == ',')
+                {
+                    currentPosition = Process(stack, currentPosition, list);
+                    continue;
+                }
+                stack.Push(c);
+            }
+
+            // process the last item
+            Process(stack, currentPosition, list);
+            return list;
+        }
+
+        private static Position Process(Stack<char> stack, Position currentPosition, HashSet<Position> positions)
+        {
+            var multiplier = 1;
+            var counter = 0;
+            char direction = '.';
+            while (stack.Count > 0)
+            {
+                var c = stack.Pop();
+                if (c >= 'A' && c <= 'Z')
+                {
+                    direction = c;
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    counter += GetIntFromChar(c) * multiplier;
+                    multiplier *= 10;
+                }
+            }
+
+            if (direction == 'L')
+            {
+                for (int i = counter; i > 0; --i)
+                {
+                    positions.Add(new Position(currentPosition.X - i, currentPosition.Y));
+                }
+            }
+            else if (direction == 'R')
+            {
+                for (int i = 1; i <= counter; ++i)
+                {
+                    positions.Add(new Position(currentPosition.X + i, currentPosition.Y));
+                }
+            }
+            else if (direction == 'U')
+            {
+                for (int i = 1; i <= counter; ++i)
+                {
+                    positions.Add(new Position(currentPosition.X, currentPosition.Y + i));
+                }
+            }
+            else if (direction == 'D')
+            {
+                for (int i = counter; i > 0; --i)
+                {
+                    positions.Add(new Position(currentPosition.X, currentPosition.Y - i));
+                }
+            }
+
+            return direction switch
+            {
+                'L' => new Position(currentPosition.X - counter, currentPosition.Y),
+                'R' => new Position(currentPosition.X + counter, currentPosition.Y),
+                'D' => new Position(currentPosition.X, currentPosition.Y - counter),
+                'U' => new Position(currentPosition.X, currentPosition.Y + counter),
+                _ => throw new ArgumentException()
+            };
+        }
+
+        private static int GetIntFromChar(char c)
+        {
+            return c switch
+            {
+                '0' => 0,
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 7,
+                '8' => 8,
+                '9' => 9,
+                _ => throw new ArgumentException(),
+            };
+        }
+
+        public struct Position : IEquatable<Position>
+        {
+            public int X { get; }
+            public int Y { get; }
+
+            public Position(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Position && Equals((Position)obj);
+            }
+
+            public bool Equals(Position other)
+            {
+                return X == other.X &&
+                       Y == other.Y;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(X, Y);
+            }
         }
 
         #endregion
