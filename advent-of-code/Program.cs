31d1439c562f8734f5b1ff2df9c543991a6942be
Francois Joly <joly.francois@gmail.com>
2019-12-02 22:00:48 -0500

    add second day of puzzle

--- advent-of-code/Program.cs
+++ advent-of-code/Program.cs
@@ -10,15 +10,18 @@ namespace advent_of_code
         static void Main(string[] args)
         {
             Day1();
+            Day2();
         }
+
+        #region day1
 
         private static void Day1()
         {
             var result1 = Day1Puzzle1();
-            Console.WriteLine($"day 1 puzzle 1: {result1}");
+            WriteResult(1, 1, result1);
 
             var result2 = Day1Puzzle2();
-            Console.WriteLine($"day 1 puzzle 2: {result2}");
+            WriteResult(1, 2, result1);
         }
 
         private static long Day1Puzzle1()
@@ -59,14 +62,114 @@ namespace advent_of_code
         private static List<long> GetDay1Puzzle1Input(string path)
         {
             var result = new List<long>();
-            using var file = new StreamReader(path);
-            var line = file.ReadLine();
-            while (line != null)
+            using (var file = new StreamReader(path))
             {
-                result.Add(long.Parse(line));
-                line = file.ReadLine();
+                var line = file.ReadLine();
+                while (line != null)
+                {
+                    result.Add(long.Parse(line));
+                    line = file.ReadLine();
+                }
+                return result;
             }
-            return result;
+        }
+
+        #endregion
+
+        #region day2
+
+        private static void Day2()
+        {
+            var input = new StreamReader("inputs/puzzle-2-1.txt").ReadToEnd();
+            var result1 = Day2Puzzle1(input);
+            WriteResult(2, 1, result1);
+
+            //var result2 = Day2Puzzle2(input);
+            //WriteResult(2, 2, result2);
+
+            Console.ReadKey();
+        }
+
+        private static int Day2Puzzle1(string input)
+        {
+            var numbers = input.Split(',').Select(i => int.Parse(i)).ToArray();
+
+            // replacements
+            numbers[1] = 12;
+            numbers[2] = 2;
+
+            var intcodeComputer = new IntcodeComputer();
+            numbers = intcodeComputer.Compute(numbers);
+            return numbers[0];
+        }
+
+        private static (int Noun, int Verb) Day2Puzzle2(string input)
+        {
+            var numbers = input.Split(',').Select(i => int.Parse(i)).ToArray();
+
+            // replacements
+            numbers[1] = 22;
+            numbers[2] = 54;
+
+            // 1 433 555
+            // [1] * 810 000 + (1870666 + [2])
+            // 19 690 720
+            // 0 0 1870666
+            // 0 1 1870667
+            // 0 2 1870668
+            // 1 0 2680666
+            // 1 1 2680667
+            // 1 2 2680668
+            // 2 0 3490666
+            // 3 0 4300666
+
+            var intcodeComputer = new IntcodeComputer();
+            numbers = intcodeComputer.Compute(numbers);
+            return (22, 54);
+        }
+
+        private static void WriteResult(int day, int puzzle, object result)
+        {
+            Console.WriteLine($"day {day} puzzle {puzzle}: {result}");
+        }
+
+        #endregion
+    }
+
+    public class IntcodeComputer
+    {
+        public int[] Compute(int[] memory)
+        {
+            var index = 0;
+            int input1, input2, output;
+            var ended = false;
+            while (!ended)
+            {
+                var value = memory[index];
+                switch (value)
+                {
+                    case 1:
+                        input1 = memory[index + 1];
+                        input2 = memory[index + 2];
+                        output = memory[index + 3];
+                        memory[output] = memory[input1] + memory[input2];
+                        index += 4;
+                        break;
+                    case 2:
+                        input1 = memory[index + 1];
+                        input2 = memory[index + 2];
+                        output = memory[index + 3];
+                        memory[output] = memory[input1] * memory[input2];
+                        index += 4;
+                        break;
+                    case 99:
+                        ended = true;
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+            return memory;
         }
     }
 }
